a0130bed8e89aa0b036c9c841ef8d174eab0a85c
longduongxd <longduongxd@gmail.com>
2020-04-10 09:04:06 +0700

    Đếm số lần ký tự xuất hiện trong chuỗi

---
+++ src/CountAppearTimeOfCharInStrong.java
@@ -0,0 +1,24 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Scanner;
+
+public class CountAppearTimeOfCharInStrong {
+    public static int countAppearTime(@NotNull String myString){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a character to check: ");
+       String chara = scan.next();
+        int count =0;
+        for (int i=0; i<myString.length();i++ ) {
+            if( chara.charAt(0) == myString.charAt(i))
+                count++;
+        }
+        return count;
+        }
+    public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a string will be checked: ");
+        String myStr = scan.nextLine();
+        System.out.println("Have "+ countAppearTime(myStr)+ " appear times in string");
+    }
+}
